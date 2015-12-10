2ccd7d31d33486bef5f5fcff7b1e8b89e4a85e98
David <davidskim89@gmail.com>
2015-12-10 09:42:42 -0800

    first commit

---
+++ test.java
@@ -0,0 +1,8 @@
+class test{
+    public static void main (String args[])
+    throws java.io.IOException {
+        System.out.println("pick a number: ");
+        int num = System.in.read();
+        System.out.println("your number is: " + num);
+        }
+    }
