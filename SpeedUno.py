c9c0472a2410a03e8cf0330301c66da70a7417be
David <davidskim89@gmail.com>
2015-12-18 14:08:29 -0800

    updated speed uno. added add player function and score updating

--- SpeedUno.py
+++ SpeedUno.py
@@ -21,6 +21,25 @@ else:
 #this creates a list of players and records names
 listofplayers = []
 for i in range(1, numofplayers + 1):
-    listofplayers.append({raw_input("What is player " + str(i) + "'s name?"):0})
-for i in listofplayers:
-    print i
+    playername = raw_input("What is player " + str(i) + "'s name?")
+    listofplayers.append({'name':playername, 'score':0})
+
+# this sets up the check to see if the game should end
+endgame = 0
+while endgame == 0:
+    for i in listofplayers:
+        score = raw_input("What is " + i['name'] + "'s score?")
+        try:
+            score = int(score)
+        except ValueError:
+            valid = 0
+            while valid == 0:
+                score = raw_input("Please enter a number for " + i['name'] + "'s score")
+                try:
+                    score = int(score)
+                except ValueError:
+                    valid = 0
+                else:
+                    valid = 1
+        i['score'] = i['score'] + score
+        print i['name'] + "'s total score is: " + str(i['score'])
