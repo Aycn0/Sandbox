25bb047eadd5411837afcc6bd6064540f8f5d04a
David <davidskim89@gmail.com>
2015-12-18 12:43:30 -0800

    delete old files

--- test.java
+++
@@ -1,8 +0,0 @@
-class test{
-    public static void main (String args[])
-    throws java.io.IOException {
-        System.out.println("pick a number: ");
-        int num = System.in.read();
-        System.out.println("your number is: " + num);
-        }
-    }
